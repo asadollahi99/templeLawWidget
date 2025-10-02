38cd18c11829bd9d92533ba1605aa67112df0bdb
Elnaz Asadollahi <tus43075@temple.edu>
2025-10-02 09:25:16 -0400

    Fix vite.config.js base path for Render deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/ctlaw/" : "/",   // dev => '/', prod => '/ctlaw/'
+  //base: mode === "production" ? "/ctlaw/" : "/",   // dev => '/', prod => '/ctlaw/'
+  base: "/",
   plugins: [react()],
   build: { outDir: "dist" },
   server: {
