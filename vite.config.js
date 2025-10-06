f4b6b0ca87e5ed2fe84d56321f75001ab8080e90
Elnaz Asadollahi <tus43075@temple.edu>
2025-10-06 10:52:33 -0400

    Change the base address

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/ctlaw/" : "/",   // dev => '/', prod => '/ctlaw/'
-  //base: "/",
+  //base: mode === "production" ? "/ctlaw/" : "/",   // dev => '/', prod => '/ctlaw/'
+  base: "/",
   plugins: [react()],
   build: { outDir: "dist" },
   server: {
